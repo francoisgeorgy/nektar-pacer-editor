312fd5efc2946673fcb33d80036d02566cf9dd6d
François Georgy <francois.georgy@gmail.com>
2018-10-26 09:08:02 +0200

    Updates

---
+++ precache-manifest.908247be01ebfe93aa6032651438e12f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b83602fa3768498b916c",
+    "url": "/pacer-editor/static/js/runtime~main.b83602fa.js"
+  },
+  {
+    "revision": "1fe8eb627931bff9505c",
+    "url": "/pacer-editor/static/js/main.1fe8eb62.chunk.js"
+  },
+  {
+    "revision": "6890d458e20822aad59b",
+    "url": "/pacer-editor/static/js/1.6890d458.chunk.js"
+  },
+  {
+    "revision": "1fe8eb627931bff9505c",
+    "url": "/pacer-editor/static/css/main.5f83414f.chunk.css"
+  },
+  {
+    "revision": "16f975d962705e82637e2585bf80bf06",
+    "url": "/pacer-editor/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/nektar-pacer-editor/precache-manifest.a7b4f7837ba6954af8caff6a2158bcbf.js"
+  "/pacer-editor/precache-manifest.908247be01ebfe93aa6032651438e12f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nektar-pacer-editor/index.html", {
+workbox.routing.registerNavigationRoute("/pacer-editor/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
