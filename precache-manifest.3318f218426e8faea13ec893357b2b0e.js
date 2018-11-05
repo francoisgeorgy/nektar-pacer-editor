970bfa3e56e2d032acdd288b26e5a0ce43e3bef9
François Georgy <francois.georgy@gmail.com>
2018-11-05 23:23:12 +0100

    Updates

---
+++ precache-manifest.3318f218426e8faea13ec893357b2b0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b83602fa3768498b916c",
+    "url": "/pacer-editor/static/js/runtime~main.b83602fa.js"
+  },
+  {
+    "revision": "74c1626ca2b6619b2f5c",
+    "url": "/pacer-editor/static/js/main.74c1626c.chunk.js"
+  },
+  {
+    "revision": "de2a04f6cf115fcb2fb1",
+    "url": "/pacer-editor/static/js/1.de2a04f6.chunk.js"
+  },
+  {
+    "revision": "74c1626ca2b6619b2f5c",
+    "url": "/pacer-editor/static/css/main.eae13cce.chunk.css"
+  },
+  {
+    "revision": "0809aee4b13a100f3220c809fb781b4e",
+    "url": "/pacer-editor/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/pacer-editor/precache-manifest.ffc6358bf583b94f8cce466d9ed8a0ec.js"
+  "/pacer-editor/precache-manifest.3318f218426e8faea13ec893357b2b0e.js"
 );
 
 workbox.clientsClaim();
